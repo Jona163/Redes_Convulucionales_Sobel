651167271dac2522f6bb4886667086b54cd419a5
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-16 15:28:25 -0600

    Update 20_formas_sobel.py

--- 20_formas_sobel.py
+++ 20_formas_sobel.py
@@ -21,3 +21,12 @@ def aplicar_variaciones_sobel(imagen):
         sobel_y = cv2.Sobel(imagen, cv2.CV_64F, 0, 1, ksize=ksize)
         variaciones.append(sobel_y)
         titulos.append(f'Sobel Y - Kernel {ksize}')
+
+     # 2. Variaciones en el orden de la derivada
+    sobel_x_2nd_order = cv2.Sobel(imagen, cv2.CV_64F, 2, 0, ksize=3)
+    variaciones.append(sobel_x_2nd_order)
+    titulos.append('Sobel X - 2nd Order')
+
+    sobel_y_2nd_order = cv2.Sobel(imagen, cv2.CV_64F, 0, 2, ksize=3)
+    variaciones.append(sobel_y_2nd_order)
+    titulos.append('Sobel Y - 2nd Order')
