aa9561c3f942e04ff39634417cf765e2212756cd
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-16 15:27:44 -0600

    Update 20_formas_sobel.py

--- 20_formas_sobel.py
+++ 20_formas_sobel.py
@@ -2,3 +2,12 @@
 # Fecha: 16 Septiembre 2024
 # Descripción: Código para procesamiento de imagenes con Sobel 20 formas.
 # GitHub: https://github.com/Jona163
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+def aplicar_variaciones_sobel(imagen):
+    variaciones = []
+    titulos = []
